e66e2736361e462440aa680c3bc3eea4f8350418
Viktor Yakubovych <vyakubovych@corelogic.com>
2022-07-03 18:23:34 +0300

    Add files

---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Cryptography;
+
+Console.WriteLine(GeneratePasswordHashUsingSalt("password",
+    new byte[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}));
+Console.ReadLine();
+
+string GeneratePasswordHashUsingSalt(string passwordText, byte[] salt)
+{
+
+    var iterate = 10000;
+    var pbkdf2 = new Rfc2898DeriveBytes(passwordText, salt, iterate);
+    byte[] hash = pbkdf2.GetBytes(20);
+
+    byte[] hashBytes = new byte[36];
+    Array.Copy(salt, 0, hashBytes, 0, 16);
+    Array.Copy(hash, 0, hashBytes, 16, 20);
+
+    var passwordHash = Convert.ToBase64String(hashBytes);
+
+    return passwordHash;
+
+}
