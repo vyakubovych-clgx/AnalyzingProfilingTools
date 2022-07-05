9a1e05c128daffe5830ee351c6f2af2c11ef689f
Viktor Yakubovych <vyakubovych@corelogic.com>
2022-07-05 17:45:35 +0300

    Try to optimize task 1

--- Task1/Task1/Program.cs
+++ Task1/Task1/Program.cs
@@ -8,12 +8,14 @@ string GeneratePasswordHashUsingSalt(string passwordText, byte[] salt)
 {
 
     var iterate = 10000;
-    var pbkdf2 = new Rfc2898DeriveBytes(passwordText, salt, iterate);
+    using var pbkdf2 = new Rfc2898DeriveBytes(passwordText, salt, iterate);
     byte[] hash = pbkdf2.GetBytes(20);
 
     byte[] hashBytes = new byte[36];
-    Array.Copy(salt, 0, hashBytes, 0, 16);
-    Array.Copy(hash, 0, hashBytes, 16, 20);
+    for (int i = 0; i < 16; i++)
+        hashBytes[i] = salt[i];
+    for (int i = 0; i < 20; i++)
+        hashBytes[16 + i] = hash[i];
 
     var passwordHash = Convert.ToBase64String(hashBytes);
 
